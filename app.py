dcb5fafd5330819e19fc57836fd677a39d08c54a
JHR1986 <jhroche1986@gmail.com>
2021-05-31 09:12:22 +0000

    Update app route for edit_task function with submit dictionary

--- app.py
+++ app.py
@@ -109,6 +109,18 @@ def add_task():
 
 @app.route("/edit_task/<task_id>", methods=["GET", "POST"])
 def edit_task(task_id):
+    if request.method == "POST":
+        submit = {
+            "category_name": request.form.get("category_name"),
+            "recipe_name": request.form.get("recipe_name"),
+            "recipe_description": request.form.get("recipe_description"),
+            "recipe_ingredients": request.form.get("recipe_ingredients"),
+            "created_by": session["user"] 
+        }
+        mongo.db.tasks.insert_one(task)
+        flash("Recipe Successfully Added")
+        return redirect(url_for("get_tasks"))
+
     task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("edit_task.html", task=task, categories=categories)
